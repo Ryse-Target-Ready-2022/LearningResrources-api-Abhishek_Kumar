1fe85b3ffee6d981f04d5fd4654d4fff6869793e
abee5hake <abhishek.kumar228@gmail.com>
2022-07-29 03:54:04 +0530

    JPA with H2

--- src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
@@ -1,94 +1,128 @@
 package com.tgt.rysetii.learningresourcesapi.entity;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class LearningResource
-{
+@Entity
+@Table(name = "learningresources")
+public class LearningResource implements Serializable {
+    @Id
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "cost_price")
     private Double costPrice;
+
+    @Column(name = "selling_price")
     private Double sellingPrice;
+
+    @Column(name = "product_status")
+    @Enumerated(EnumType.STRING)
     private LearningResourceStatus productStatus;
+
+    @Column(name = "created_date")
     private LocalDate createdDate;
+
+    @Column(name = "published_date")
     private LocalDate publishedDate;
+
+    @Column(name = "retired_date")
     private LocalDate retiredDate;
 
-    public LearningResource() {
-    }
+        public LearningResource() {
+        }
 
-    public LearningResource(Integer learningResourceId, String productName, Double costPrice, Double sellingPrice, LearningResourceStatus learningResourceStatus, LocalDate createdDate, LocalDate publishedDate, LocalDate retiredDate) {
-        this.id = learningResourceId;
-        this.name = productName;
-        this.costPrice = costPrice;
-        this.sellingPrice = sellingPrice;
-        this.productStatus = learningResourceStatus;
-        this.createdDate = createdDate;
-        this.publishedDate = publishedDate;
-        this.retiredDate = retiredDate;
-    }
+        public LearningResource(Integer learningResourceId, String productName, Double costPrice, Double sellingPrice, LearningResourceStatus learningResourceStatus, LocalDate createdDate, LocalDate publishedDate, LocalDate retiredDate) {
+            this.id = learningResourceId;
+            this.name = productName;
+            this.costPrice = costPrice;
+            this.sellingPrice = sellingPrice;
+            this.productStatus = learningResourceStatus;
+            this.createdDate = createdDate;
+            this.publishedDate = publishedDate;
+            this.retiredDate = retiredDate;
+        }
 
-    public Integer getLearningResourceId() {
-        return id;
-    }
+        public Integer getLearningResourceId() {
+            return id;
+        }
 
-    public void setLearningResourceId(Integer learningResourceId) {
-        this.id = learningResourceId;
-    }
+        public void setLearningResourceId(Integer learningResourceId) {
+            this.id = learningResourceId;
+        }
 
-    public String getProductName() {
-        return name;
-    }
+        public String getProductName() {
+            return name;
+        }
 
-    public void setProductName(String productName) {
-        this.name = productName;
-    }
+        public void setProductName(String productName) {
+            this.name = productName;
+        }
 
-    public Double getCostPrice() {
-        return costPrice;
-    }
+        public Double getCostPrice() {
+            return costPrice;
+        }
 
-    public void setCostPrice(Double costPrice) {
-        this.costPrice = costPrice;
-    }
+        public void setCostPrice(Double costPrice) {
+            this.costPrice = costPrice;
+        }
 
-    public Double getSellingPrice() {
-        return sellingPrice;
-    }
+        public Double getSellingPrice() {
+            return sellingPrice;
+        }
 
-    public void setSellingPrice(Double sellingPrice) {
-        this.sellingPrice = sellingPrice;
-    }
+        public void setSellingPrice(Double sellingPrice) {
+            this.sellingPrice = sellingPrice;
+        }
 
-    public LearningResourceStatus getLearningResourceStatus() {
-        return productStatus;
-    }
+        public LearningResourceStatus getLearningResourceStatus() {
+            return productStatus;
+        }
 
-    public void setLearningResourceStatus(LearningResourceStatus learningResourceStatus) {
-        this.productStatus = learningResourceStatus;
-    }
+        public void setLearningResourceStatus(LearningResourceStatus learningResourceStatus) {
+            this.productStatus = learningResourceStatus;
+        }
 
-    public LocalDate getCreatedDate() {
-        return createdDate;
-    }
+        public LocalDate getCreatedDate() {
+            return createdDate;
+        }
 
-    public void setCreatedDate(LocalDate createdDate) {
-        this.createdDate = createdDate;
-    }
+        public void setCreatedDate(LocalDate createdDate) {
+            this.createdDate = createdDate;
+        }
 
-    public LocalDate getPublishedDate() {
-        return publishedDate;
-    }
+        public LocalDate getPublishedDate() {
+            return publishedDate;
+        }
 
-    public void setPublishedDate(LocalDate publishedDate) {
-        this.publishedDate = publishedDate;
-    }
+        public void setPublishedDate(LocalDate publishedDate) {
+            this.publishedDate = publishedDate;
+        }
 
-    public LocalDate getRetiredDate() {
-        return retiredDate;
-    }
+        public LocalDate getRetiredDate() {
+            return retiredDate;
+        }
 
-    public void setRetiredDate(LocalDate retiredDate) {
-        this.retiredDate = retiredDate;
+        public void setRetiredDate(LocalDate retiredDate) {
+            this.retiredDate = retiredDate;
+        }
+
+        @Override
+        public String toString() {
+            return "LearningResource{" +
+                    "learningResourceId=" + id +
+                    ", learningResourceName='" + name + '\'' +
+                    ", costPrice=" + costPrice +
+                    ", sellingPrice=" + sellingPrice +
+                    ", learningResourceStatus=" + productStatus +
+                    ", createdDate=" + createdDate +
+                    ", publishedDate=" + publishedDate +
+                    ", retiredDate=" + retiredDate +
+                    '}';
+        }
     }
-}
 
